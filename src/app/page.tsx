33892ae9391af23eae5a2363cecfa1b5d05d50d7
mizou9494 <betarmizou@gmail.com>
2024-11-19 17:34:55 +0100

    the signIn and signUp ui are done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import {Button} from "@/components/ui/button";
+import { AuthScreen } from "@/app/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="h-screen grid place-content-center">
-      <h1 className="text-3xl text-rose-600 font-bold flex items-center justify-center">This is the Home Page <Button variant="slack">Button</Button> </h1>
-    </div>
+    <AuthScreen />
   )
 }
